bd80eb52f3c6b122738ae6962c0b33ecee69d972
humbl3LilaS <sabishinekobebe@gmail.com>
2024-11-14 22:16:32 +0630

    add: route guard middleware

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,30 @@
-export {auth as middleware} from '@/auth';
+import {NextRequest, NextResponse} from "next/server";
+import {getToken} from "@auth/core/jwt";
+
+export {auth} from '@/auth';
+
+export async function middleware(request: NextRequest) {
+
+    const publicPath = ["/sign-in", "/sign-up"];
+    const pathname = request.nextUrl.pathname;
+
+    const isProtectRoute = !publicPath.some((path) => path === pathname);
+
+    if (!isProtectRoute) {
+        return NextResponse.next();
+    }
+
+    const token = await getToken({req: request, secret: process.env.AUTH_SECRET});
+    if (token) {
+        return NextResponse.next();
+    }
+
+    const loginUrl = new URL("/login", request.url)
+
+    return NextResponse.redirect(loginUrl)
+}
+
+
+export const config = {
+    matcher: ["/:path*"]
+}
